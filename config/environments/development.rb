f62ad1bb1b37f9990954bebb8d4a928a20546ad5
Amy Nicholson <missamynicholson@gmail.com>
2016-05-26 17:51:32 +0100

    AWS S3 - picture upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => "s3-eu-west-1.amazonaws.com",
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
